6f115ad77abb8d027dab4e474476858717a943b8
Karthik Pullela <karthik.pullelas@gmail.com>
2018-11-09 23:31:58 -0800

    vid4: Forgot to declare function with no type

--- port.cpp
+++ port.cpp
@@ -11,11 +11,11 @@ Port8Bit::Port8Bit(uint16_t portnumber) : Port(portnumber) {};
 
 Port8Bit::~Port8Bit() {};
 
-Port8Bit::Write(uint8_t data) {
+void Port8Bit::Write(uint8_t data) {
   __asm__ volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
 }
 
-Port8Bit::Read() {
+uint8_t Port8Bit::Read() {
   uint8_t result;
   __asm__ volatile("inb %1, %0" : : "=a" (result) : "Nd" (portnumber));
   return result;
@@ -26,7 +26,7 @@ Port8BitSlow::Port8BitSlow(uint16_t portnumber) : Port8Bit(portnumber);
 
 Port8BitSlow::~Port8BitSlow() {};
 
-Port8BitSlow::Write(uint8_t data) {
+void Port8BitSlow::Write(uint8_t data) {
   __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (data), "Nd" (portnumber));
 }
 
@@ -35,11 +35,11 @@ Port16Bit::Port16Bit(uint16_t portnumber) : Port(portnumber) {};
 
 Port16Bit::~Port16Bit() {};
 
-Port16Bit::Write(uint16_t data) {
+void Port16Bit::Write(uint16_t data) {
   __asm__ volatile("outw %0, %1" : : "a" (data), "Nd" (portnumber));
 }
 
-Port16Bit::Read() {
+uint16_t Port16Bit::Read() {
   uint8_t result;
   __asm__ volatile("inw %1, %0" : : "=a" (result) : "Nd" (portnumber));
   return result;
@@ -50,11 +50,11 @@ Port32Bit::Port32Bit(uint16_t portnumber) : Port(portnumber) {};
 
 Port32Bit::~Port32Bit() {};
 
-Port32Bit::Write(uint32_t data) {
+void Port32Bit::Write(uint32_t data) {
   __asm__ volatile("outl %0, %1" : : "a" (data), "Nd" (portnumber));
 }
 
-Port32Bit::Read() {
+uint32_t Port32Bit::Read() {
   uint8_t result;
   __asm__ volatile("inl %1, %0" : : "=a" (result) : "Nd" (portnumber));
   return result;
